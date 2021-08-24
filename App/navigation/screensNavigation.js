fbb4c60bdb66dfc98339d95753732a33451fc060
JuiceWAH10 <jjamora121014@gmail.com>
2021-08-25 01:44:10 +0800

    installed libraries
    also removed owners screens and edited nav

--- App/navigation/screensNavigation.js
+++ App/navigation/screensNavigation.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createStackNavigator, createAppContainer } from "@react-navigation/stack";
 import { customerBottomTabs } from './bottomTabs';
 
-
 import QRCodeScanner from '../screens/QRCodeScanner';
 
 // Customer Screens
@@ -14,32 +13,13 @@ import ShopItemsQR from '../screens/customer/shopItems/shopItemsQR';
 import RewardItemsQR from '../screens/customer/rewardItems/rewardItemsQR';
 import CustomerEditProfile from '../screens/customer/profile/customerEditProfile';
 
-// Client Screens
-import ClientHomepage from '../screens/owners/clientHomepage';
-import ClientProductAdd from '../screens/owners/clientProduct/clientProductAdd';
-import ClientProductEdit from '../screens/owners/clientProduct/clientProductEdit';
-import ClientProductList from '../screens/owners/clientProduct/clientProductList';
-import ClientRewardAdd from '../screens/owners/clientReward/clientRewardAdd';
-import ClientRewardEdit from '../screens/owners/clientReward/clientRewardEdit';
-import ClientRewardList from '../screens/owners/clientReward/clientRewardList';
-import ClientSukiList from '../screens/owners/clientSuki/clientSukiList';
-import ClientEditProfile from '../screens/owners/clientProfile/clientEditProfile';
-
 const Stack = createStackNavigator();
 
 export default Screens = () => {
   return(
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="customerShops" component={ClientHomepage} />  
-      
       <Stack.Screen name="QRCodeScanner" component={QRCodeScanner} />  
-      {/* Added Vincent 
-      <Stack.Screen name="explore" component={Explore} />
-      <Stack.Screen name="rewards" component={Rewards} />
-      <Stack.Screen name="shops" component={Shops} />
-      <Stack.Screen name="profile" component={Profile} />
-      <Stack.Screen name="clientHomepage" children={customerBottomTabs} />  
-      */}
       <Stack.Screen name="shopItems" component={ShopItems} />
       <Stack.Screen name="shopItemsCart" component={ShopItemsCart} />  
       <Stack.Screen name="shopItemsQR" component={ShopItemsQR} />   
@@ -47,18 +27,6 @@ export default Screens = () => {
       <Stack.Screen name="rewardItemsCart" component={RewardItemsCart} />
       <Stack.Screen name="rewardItemsQR" component={RewardItemsQR} />
       <Stack.Screen name="customerEditProfile" component={CustomerEditProfile} />
-
-      
-      <Stack.Screen name="clientProductAdd" component={ClientProductAdd} />
-      <Stack.Screen name="clientProductEdit" component={ClientProductEdit} />
-      <Stack.Screen name="clientProductList" component={ClientProductList} />
-      <Stack.Screen name="clientRewardAdd" component={ClientRewardAdd} />
-      <Stack.Screen name="clientRewardEdit" component={ClientRewardEdit} />
-      <Stack.Screen name="clientRewardList" component={ClientRewardList} />
-      <Stack.Screen name="clientSukiList" component={ClientSukiList} />
-      <Stack.Screen name="clientEditProfile" component={ClientEditProfile} />
-      {/* End Added Vincent */}
-
     </Stack.Navigator>
     
   );
